ec624941bed2d8edef7a9a213628b981adc0290c
Mosaab Emam <mosaab.emam123@gmail.com>
2023-01-26 13:43:38 +0000

    :construction: Making it my own

---
+++ components/DelightApp.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="w-12 h-12 .shadow shadow-amber">
+    {{ props.name }}
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="text-9xl text-teal mb8">
+      Delight.SD
+    </div>
+    <div class="text-xl">
+      عائلة تطبيقات سودانية. مقدمة لكم بواسطة <a href="https://mosaabemam.com" class="text-teal">مصعب الإمام</a>
+    </div>
     <Logos mb-6 />
     <Suspense>
       <PageView />
